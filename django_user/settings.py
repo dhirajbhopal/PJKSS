b097a67d5f0ed360ee1c388c916bc0baf82cc873
dhirajbhopal <dhirajpatel08@gmail.com>
2025-10-03 18:03:28 +0530

    member list name addednew

--- django_user/settings.py
+++ django_user/settings.py
@@ -153,14 +153,14 @@ AUTH_USER_MODEL = "core.User"
 
 
 #Email Setup
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' 
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' 
 #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'pjkssinfo@gmail.com'
-EMAIL_HOST_PASSWORD = 'bwqpbkcdutyqheoi'
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_PORT = 587
+#EMAIL_HOST_USER = 'pjkssinfo@gmail.com'
+#EMAIL_HOST_PASSWORD = 'bwqpbkcdutyqheoi'
+#EMAIL_USE_TLS = True
+#DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 
